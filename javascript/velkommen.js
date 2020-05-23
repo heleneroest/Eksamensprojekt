69f9c161f0fa177f9d8fe8eb674224b8cbeceb29
BrokeBird <johnprasmussen@hotmail.com>
2020-05-23 11:55:49 +0200

    1. upload
    Jeg har her oprettet alle mapperne for de enkelte Html, Css & JavaScript sider som jeg skal kode i gruppen.

---
+++ javascript/velkommen.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("toggleMenu"); // local variable
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+i = 0;
+var bg = ["red", "rgb(48, 49, 52"]; // array, loop igennem
+function colorChange(which) {
+  which.style.backgroundColor = bg[i++ % bg.length];
+}
