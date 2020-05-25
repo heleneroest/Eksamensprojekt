64c3423f97e77b55730e475e6607bfa365e7e000
heleneroest <60442736+heleneroest@users.noreply.github.com>
2020-05-25 13:45:57 +0200

    Alle sider sat sammen

--- javascript/mark.js
+++ javascript/mark.js
@@ -7,12 +7,4 @@ function myFunction() {
     x.style.display = "block";
   }
 }
-
-// Helene markering af valg
-i = 0;
-var bg = ["rgba(255, 0, 0, 0.6)", "rgb(48, 49, 52"]; // array, loop igennem
-function colorChange(which){
-  which.style.backgroundColor = bg[i++ % bg.length];
-}
-
 // Helene slut
