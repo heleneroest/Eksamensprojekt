df4bbaf23e5660a1b48eeb5bc61f82fc65e03814
heleneroest <60442736+heleneroest@users.noreply.github.com>
2020-05-25 09:32:58 +0200

    -

--- javascript/helene.js
+++ javascript/helene.js
@@ -1,15 +1,14 @@
-function myFunction() {
+function myFunction(){
   var x = document.getElementById("toggleMenu"); // local variable
-  if (x.style.display === "block") {
+  if (x.style.display == "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
 }
 
-
 i = 0;
-var bg = ["red", "rgb(48, 49, 52"]; // array, loop igennem
-function colorChange(which) {
+var bg = ["rgba(255, 0, 0, 0.6)", "rgb(48, 49, 52"]; // array, loop igennem
+function colorChange(which){
   which.style.backgroundColor = bg[i++ % bg.length];
 }
--- javascript/vaelg-palletype.js
+++ javascript/vaelg-palletype.js
@@ -6,10 +6,3 @@ function myFunction() {
     x.style.display = "block";
   }
 }
-
-
-i = 0;
-var bg = ["red", "rgb(48, 49, 52"]; // array, loop igennem
-function colorChange(which) {
-  which.style.backgroundColor = bg[i++ % bg.length];
-}
